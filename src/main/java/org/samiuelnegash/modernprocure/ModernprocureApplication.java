195cea28cd56b5c2c1b42cf3ed0a63183507d7df
sami16mk <samiuelmichael@gmail.com>
2022-09-15 23:18:44 -0700

    Initial commit

---
+++ src/main/java/org/samiuelnegash/modernprocure/ModernprocureApplication.java
@@ -0,0 +1,13 @@
+package org.samiuelnegash.modernprocure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernprocureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernprocureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/samiuelnegash/modernprocure/ModernprocureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.samiuelnegash.modernprocure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernprocureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
